30e8d6513edb143da3039843fa7d1d73f3f43b1e
gahwa17 <s24527109@gmail.com>
2022-03-09 01:57:23 +0800

    safe_version

--- EX1_1.c
+++ EX1_1.c
@@ -6,10 +6,8 @@
 #define MAX_PATN 100
 #define MAX_ARC 4095
 
-void read_line();
-void read_pattern();  //read pattern,replacement and parameter
+void read_pattern();
 int check_parameter(); // check whether parameter is valid
-void read_article();
 
 int main(void){
     char arc[MAX_ARC];
@@ -17,30 +15,26 @@ int main(void){
     char *new;
     char *para;
 
-    //Init article
-    for (int i = 0; i < MAX_ARC;i++)
-        arc[i] = '\0';
-
-    fprintf(stderr, "Enter pattern, replacement, and at most one parameter: ");
+    fprintf(stderr, "Enter pattern, replacement, and at most one parameter: \n");
 
     char pattern[80];
     char delim[10] = " !?_\n\t,.";
 
-    read_line(pattern, 100);
+    read_pattern(pattern, 100);
     
     old = strtok(pattern, delim);
     new = strtok(NULL, delim);
     para = strtok(NULL, delim);
 
-    printf("old:%s\n", old);
-    printf("new:%s\n", new);
-    printf("para:%s\n", para);
+    // printf("old:%s\n", old);
+    // printf("new:%s\n", new);
+    // printf("para:%s\n", para);
 
     int oldlen = strlen(old);
     int newlen = strlen(new);
 
-    printf("oldlen: %d\n", oldlen);
-    printf("newlen: %d\n", newlen);
+    // printf("oldlen: %d\n", oldlen);
+    // printf("newlen: %d\n", newlen);
 
 
     
@@ -48,25 +42,29 @@ int main(void){
     if(check_parameter(para) == FALSE)
         fprintf(stderr, "The input format: string1 string2 [parameter]");
     else{
-        fprintf(stderr, "Enter the article: ");
+        fprintf(stderr, "Enter the article: \n");
 
-        while(fgets(arc,MAX_PATN,stdin) != NULL){
-            printf("%s\n", arc);
-            /*
+        while(fgets(arc,MAX_ARC,stdin) != NULL){
+
+            // printf("cur's arc:%s", arc);
+            
+            arc[strlen(arc) - 1] = '\0';
+
+            
             char skip[10] = " !?_\n\t,.";
             char *token;
             char *needle;
             token = strtok(arc, skip);
 
             while(token != NULL){
-                printf("cur token is: %s\n", token);
                 int cnt = 0;
                 int tokenlen = strlen(token);
 
                 needle = strstr(token, old);
                 
                 if(needle != NULL){
-                    printf("cur needle is: %s\n", needle);
+                    // printf("cur's token is: %s\n", token);
+                    // printf("cur needle is: %s\n", needle);
                     //count how many oldword exist in every token 
                     for (int i = 0; i < strlen(token); i++){
                         if(strstr(&token[i],old) == &token[i]){
@@ -74,7 +72,7 @@ int main(void){
                             i = i + oldlen;
                         }
                     }
-                    printf("cnt : %d\n", cnt);
+                    // printf("cnt : %d\n", cnt);
 
                     //create new str to replace old with new
                     int reslen = tokenlen + (newlen - oldlen) * cnt + 1;
@@ -85,50 +83,34 @@ int main(void){
 
                     while(i < reslen){
                         for ( ; ptr < needle; ptr++){
-                        res[i] = *ptr;
-                        i++;
+                            res[i] = *ptr;
+                            i++;
                         }
                         for (int j = 0; j < newlen;j++){
-                        res[i] = new[j];
-                        i++;
+                            res[i] = new[j];
+                            i++;
                         }
                         
                         for (int k = 0; k < oldlen;k++)
-                        ptr++;
+                            ptr++;  
 
                         for ( ; *ptr!='\0' ; ptr++){
-                        res[i] = *ptr;
-                        i++;
+                            res[i] = *ptr;
+                            i++;
                         }
                     }
                     
                     res[reslen - 1] = '\0';
-                    printf("res: %s\n", res);
+                    printf("%s\n", res);
                 }
                 token = strtok(NULL, skip);
             }
-            */
+            
         }
     }
 }
 
-//Discard
-void read_pattern(char *old,char *new,char *para){
-    char pattern[80];
-    char delim[10] = " !?_\n\t,.";
-
-    read_line(pattern, 100);
-    
-    old = strtok(pattern, delim);
-    new = strtok(NULL, delim);
-    para = strtok(NULL, delim);
-
-    printf("old:%s\n", old);
-    printf("new:%s\n", new);
-    printf("para:%s\n", para);
-}
-
-void read_line(char str[], int n)
+void read_pattern(char str[], int n)
 {
     int ch, i = 0;
     while ((ch = getchar()) != '\n'){
@@ -149,11 +131,6 @@ int check_parameter(char para[]){
     return flag;
 }
 
-void read_article(char str[]){
-    int ch;
-    while ( (ch = getchar()) != EOF){
-        while(ch != '\n'){
+void tolowcase(){
 
-        }
-    }
 }
