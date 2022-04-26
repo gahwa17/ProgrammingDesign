3fa82ffe7770fc41f5df3849e89eb4997a289f21
gahwa17 <s24527109@gmail.com>
2022-04-26 15:58:36 +0800

    EX5

--- Exercise(110-2)/EX5/Source/link_list.c
+++ Exercise(110-2)/EX5/Source/link_list.c
@@ -12,39 +12,90 @@ void my_insert(struct node* HEAD, int ID, char *Name, int node_id){
 	struct node* ptr = HEAD;
 	for( ; ptr != NULL ; ptr = ptr->next){
 		if(ptr->id == ID){
-			flag == TRUE;
+			flag = TRUE;
 			break;
 		}
 	}
 	if(flag == FALSE)
-		printf("INVALID OPERATION");
+		printf("INVALID OPERATION\n");
 	//if ID exist,then create a new_node and insert after it
 	else{
 		struct node *new_node = malloc(sizeof(struct node));
 		strcpy(new_node->name, Name);
 		new_node->id = node_id;
-		ptr->next = new_node;
+
+		//if there is already other node, then keep it as tmp
+		if(ptr->next != NULL){
+			struct node *tmp = ptr->next;
+			ptr->next = new_node;
+			new_node->next = tmp;
+		}
+		//if there is no node,then just insert it
+		else{
+			ptr->next = new_node;
+		}
 	}
 }
 
+
+// Delete the node which store the Name.
+// If it doesn’t exit such Name, print “INVALID OPERATION”.
 void my_delete(struct node* HEAD, char *Name){
-	// Delete the node which store the Name.
-	// If it doesn’t exit such Name, print “INVALID OPERATION”.
+	int flag = FALSE;
+	struct node* ptr = HEAD;
+	//Find target node's former node
+	while(ptr->next != NULL){
+		if(strcmp((ptr->next)->name, Name) != 0)
+			ptr = ptr->next;
+		else{
+			flag = TRUE;
+			break;
+		}
+	}
+
+	if(flag == FALSE)
+		printf("INVALID OPERATION\n");
+	else{
+		struct node* to_delete = ptr->next;
+		printf("Former is :%d %s\n",ptr->id,ptr->name);
+		printf("To delete is :%d %s\n",to_delete->id,to_delete->name);
+		ptr->next = to_delete->next;
+		free(to_delete);
+	}
 
 }
+
+// Swap the node which store Name1 and Name2
+// If either of the node doesn’t exit, print “INVALID OPERATION”
+// NOTICE: don't just swap the node's value
+// You need to break the link and recombine it
 void my_swap(struct node *HEAD, char *Name1, char *Name2){
-	// Swap the node which store Name1 and Name2
-	// If either of the node doesn’t exit, print “INVALID OPERATION”
-	// NOTICE: don't just swap the node's value
-	// You need to break the link and recombine it
+
 }
+
+// Print the id of the corresponding Name.
+// If Name doesn’t exit, print “INVALID OPERATION”
 void my_print_id(struct node *HEAD, char *Name){
-	// Print the id of the corresponding Name.
-	// If Name doesn’t exit, print “INVALID OPERATION”
+	for( ; HEAD != NULL ; HEAD = HEAD->next){
+		if(strcmp(HEAD->name, Name) == 0){
+			printf("%d\n",HEAD->id);
+			break;
+		}
+	}
+	printf("INVALID OPERATION\n");
 }
+
+
+// Print the name of the corresponding ID
+// If ID doesn’t exit, print “INVALID OPERATION”
 void my_print_name(struct node *HEAD, int ID){
-	// Print the name of the corresponding ID
-	// If ID doesn’t exit, print “INVALID OPERATION”
+	for( ; HEAD != NULL ; HEAD = HEAD->next){
+	if(HEAD->id == ID){
+		printf("%s\n",HEAD->name);
+		break;
+		}		
+	}
+	printf("INVALID OPERATION\n");
 }
 
 // Print out all the information in the linked list orderly
